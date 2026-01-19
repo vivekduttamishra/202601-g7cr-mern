0c8a5ceded15114f644d884c424dec93dc774fc7
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2026-01-19 16:56:56 +0530

    day#1 update#1

---
+++ basic-js/demo04.js
@@ -0,0 +1,4 @@
+console.log('Hello World on Console!');
+document.write('Hello World on Document!')
+document.getElementById('output').innerHTML = 'Hello World in Div!';
+console.log('End of Script');
