bab5485c026d8020d268fd1ddd12ad6d03a3a574
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2026-02-04 11:37:29 +0530

    day#12 update#3

---
+++ js-unit-test-mocha-chai/test/prime-promise.test.js
@@ -0,0 +1,17 @@
+
+
+describe('prime-promise',function(){
+
+    it('should return 25 primes under 100',function(){
+
+    })
+
+    it('should return right number of primes under 500,000',function(){
+
+    })
+
+    it('should return error for invalid range',function(){
+
+    })
+
+})
