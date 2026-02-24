972afd2457e722d0ea9f5b873a952b29c3c4406f
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2026-02-24 16:33:45 +0530

    day#25 update#4

--- nodejs/nodejs_basics/fs/fs06_copyUsingStreams.js
+++ nodejs/nodejs_basics/fs/fs06_copyUsingStreams.js
@@ -2,80 +2,55 @@ import fs, { createReadStream, createWriteStream } from 'fs'
 import { EventEmitter } from 'events'
 
 
-function copyFile(srcFile, targetFile){
-    let event=new EventEmitter() //we will inform you when file is fully copied.
+function copyFile(srcFile, targetFile) {
+    const event = new EventEmitter()
 
-    let buffer=""; //we will save a chunk a time
-    let packets=[];
-    let firstChunk=true;
-    let readBytes=0;
-    let writeBytes=0;
-    let readCount=0;
-    let writeCount=0;
+    let readBytes = 0
+    let writeBytes = 0
+    let readCount = 0
+    let writeCount = 0
 
-    let source = createReadStream(srcFile, 'utf-8')
-    let target= createWriteStream(targetFile, 'utf-8')
-    let readOver=false;
+    const source = createReadStream(srcFile, { encoding: 'utf8' })
+    const target = createWriteStream(targetFile, { encoding: 'utf8' })
+
     source
-        .on('error',(error)=>{
-            process.stderr.write(`Error reading source: ${error.message}\n`)
-            target.close();
+        .on('error', err => {
+            process.stderr.write(`Error reading source: ${err.message}\n`)
+            target.destroy(err)
         })
-        .on('data', data =>{
-            readBytes+=data.length
-            readCount++;
+        .on('data', chunk => {
+            readBytes += chunk.length
+            readCount++
             process.stdout.write('+')
-            if(firstChunk){
-                writeBytes+=data.length
-                writeCount++
-                process.stdout.write('-')
-                target.write(data) //write and wait for write to finish
-                firstChunk=false;
-            } else{
-                //i am not sure if file can handle more write
-                //so I will save the data in memory till file is ready to write
-                buffer+=data;
+
+            const canWriteMore = target.write(chunk)
+            writeBytes += chunk.length
+            writeCount++
+            process.stdout.write('-')
+
+            if (!canWriteMore) {
+                source.pause()
             }
         })
-        .on('close',()=>{
-            //we read the data
-            //now we can close the target file.
-            readOver=true; 
-            process.stdout.write(" X ")
-        });
+        .on('end', () => {
+            // no more data, ask target to finish when its buffer is flushed
+            target.end()
+        })
 
     target
-        .on('error',(error)=>process.stderr.write(`Error writing target: ${error.message}`))
-        .on('close',()=>{
-            console.log('target file is closed')
-            event.emit('done',{readBytes,readCount, writeBytes,writeCount})  
+        .on('drain', () => {
+            source.resume()
         })
-        .on('drain',()=>{
-            //previous right has finished
-            //I am ready for more
-            process.stdout.write("?")
-            if(readOver && buffer.length===0){
-                console.log('target is closed')
-                target.close();
-                
-            }
-            
-           while(!buffer.length)
-            ;
-
-
-            writeBytes+=buffer.length
-            let w= buffer;
-            target.write(w); //write this piece of information 
-            process.stdout.write('-')
-            buffer="" //now buffer is empty
-            writeCount++;
-            
-        });
+        .on('error', err => {
+            process.stderr.write(`Error writing target: ${err.message}\n`)
+        })
+        .on('close', () => {
+            event.emit('done', { readBytes, readCount, writeBytes, writeCount })
+        })
 
     return event
-
 }
+
 
 copyFile('./fs/very-big.txt', './fs/very-big-2.txt')
     .on('done', console.log)
